4dc70994347af0f46062392ce17bc9adf9f71a93
Francisco Javier Diaz Martin <fjdiaz@stratio.com>
2018-10-22 17:54:10 +0200

    Initial commit. Added babel compile.

---
+++ index.js
@@ -0,0 +1,11 @@
+import http from 'http';
+
+const requestHandler = function (req, res) {
+    res.writeHead(200, {
+        'Content-Type': 'text/plain'
+    })
+    res.end('Hello')
+}
+
+const server = http.createServer(requestHandler);
+server.listen(8090);
