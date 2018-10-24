eeff4fb62bc72df7eff0bfbab5cfd87718edef66
Francisco Javier Diaz Martin <fjdiaz@stratio.com>
2018-10-24 13:00:28 +0200

    Complete Empty Payload scenario

--- dist/index.js
+++ dist/index.js
@@ -7,6 +7,16 @@ var _http = _interopRequireDefault(require("http"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function requestHandler(req, res) {
+  if (req.method === 'POST' && req.url === '/users') {
+    res.writeHead(400, {
+      'Content-TYpe': 'application/json'
+    });
+    res.end(JSON.stringify({
+      message: 'Payload should not be empty'
+    }));
+    return;
+  }
+
   res.writeHead(200, {
     'Content-Type': 'text/plain'
   });
--- spec/cucumber/steps/index.js
+++ spec/cucumber/steps/index.js
@@ -4,9 +4,10 @@ import superagent from 'superagent';
 let request;
 let result;
 let error;
+let payload;
 
 When('the client creates a POST request to /users', function () {
-  request = superagent('POST', 'localhost:8080/users');
+  request = superagent('POST', 'localhost:8090/users');
 });
 
 When('attaches a generic empty payload', function () {
@@ -31,10 +32,26 @@ Then('our API should respond with a 400 HTTP status code', function () {
   }
 });
 
-Then('the payload of the response should be a JSON object', function (callback) {
-  callback(null, 'pending');
+Then('the payload of the response should be a JSON object', function () {
+  const response = result || error;
+
+  // Check Content-Type header
+  const contentType = response.headers['Content-Type'] || response.headers['content-type'];
+  if (!contentType || !contentType.includes('application/json')) {
+    throw new Error('Response not of Content-Type application/json');
+  }
+
+  // Check it is valid JSON
+  try {
+    payload = JSON.parse(response.text);
+  } catch (e) {
+    throw new Error('Response not a valid JSON object'); 
+  } 
+  
 });
 
-Then('contains a message property which says "Payload should not be empty"', function (callback) {
-  callback(null, 'pending');
+Then('contains a message property which says "Payload should not be empty"', function () {
+  if (payload.message !== 'Payload should not be empty') {
+    throw new Error();
+  }
 });
