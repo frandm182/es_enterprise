ddffac36e648b2a5efcab51b056219c1ac4bd923
Francisco Javier Diaz Martin <fjdiaz@stratio.com>
2018-10-25 08:12:59 +0200

    Added payload too large response

--- src/index.js
+++ src/index.js
@@ -4,8 +4,15 @@ import http from 'http';
 function requestHandler(req, res) {
   if (req.method === 'POST' && req.url === '/users') {
     const payloadData = [];
+    const PAYLOAD_LIMIT = 1e6;
     req.on('data', (data) => {
       payloadData.push(data);
+      const bodyString = Buffer.concat(payloadData).toString();
+      if (bodyString.length > PAYLOAD_LIMIT) {
+        res.writeHead(413, { 'Content-Type': 'text/plain' });
+        res.end();
+        res.connection.destroy();
+      }
     });
 
     req.on('end', () => {
